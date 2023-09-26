d22e4edff938a0a2589d1c72175cdc55df490d34
Keely001 <thindiuke@gmail.com>
2023-09-26 13:18:05 +0300

    corrections

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,24 +1,43 @@
 #include "sort.h"
 
 /**
+ * swap_int - two integers swap.
+ * @a: first integer.
+ * @b: second integer.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * shell_sort - shell sort algorithm
  * @array: array of integers.
  * @size: size of array.
  */
- void shell_sort(int *array, size_t size) {
-    int interv = 1;
-    while (interv <= size / 3)
-        interv = interv * 3 + 1;
-    while (interv > 0) {
-        for (int a = interv; a < size; a++) {
-            int temp = array[a];
-            int j;
-            for (j = a; j >= interv && array[j - interv] > temp; j -= interv) {
-                array[j] = array[j - interv];
-            }
-            array[j] = temp;
-        }
-        interv = (interv - 1) / 3;
-        print_array(array, size);
-    }
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_int(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
 }
