19c96b2afb07ed60f48ebcbd8f47809feb731ddc
Keely001 <thindiuke@gmail.com>
2023-09-26 01:18:08 -0700

    task o removal of spaces

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,24 +9,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t temp, i, j;
-    int signal;
-    
-    for (i = 0; i < size ; i++)
-    {
-        signal = 0;
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array (array, size);
-                signal = 1;
-            }
-        }
-        if (signal == 0)
-            break;
-    }
+	size_t temp, i, j;
+	int signal;
+
+	for (i = 0; i < size ; i++)
+	{
+		signal = 0;
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				signal = 1;
+			}
+		}
+		if (signal == 0)
+			break;
+	}
 }
