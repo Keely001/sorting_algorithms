9d1798e96cfbbbca8e9a0d6f10ff5afaa9b0fd98
Keely001 <thindiuke@gmail.com>
2023-09-26 01:34:53 -0700

    remove space

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,31 +6,28 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr, *nxt;
+	listint_t *curr, *nxt;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-    curr = (*list)->next;
-    while (curr != NULL) {
-        nxt = curr->next;
-        while (curr->prev != NULL && curr->n < curr->prev->n)
-        {
-            curr->prev->next = curr->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	curr = (*list)->next;
+	while (curr != NULL) {
+		nxt = curr->next;
+		while (curr->prev != NULL && curr->n < curr->prev->n)
+		{
+			curr->prev->next = curr->next;
 
-            if (curr->next != NULL)
-                curr->next->prev = curr->prev;
-
-            curr->next = curr->prev;
-            curr->prev = curr->next->prev;
-            curr->next->prev = curr;
-            if (curr->prev == NULL)
-                *list = curr;
-
-            else
-                curr->prev->next = curr;
-
-            print_list(*list);
-        }
-        curr = nxt;
-    }
+			if (curr->next != NULL)
+				curr->next->prev = curr->prev;
+			curr->next = curr->prev;
+			curr->prev = curr->next->prev;
+			curr->next->prev = curr;
+			if (curr->prev == NULL)
+				*list = curr;
+			else
+				curr->prev->next = curr;
+			print_list(*list);
+		}
+		curr = nxt;
+	}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,21 +5,23 @@
  * @size: size of array
  * Return: None.
  */
- void selection_sort(int *array, size_t size) {
-    size_t i, j, min_ind, tmp;
-
-    for (i = 0; i < size - 1; i++) {
-        min_ind = i;
-        for (j = i + 1; j < size; j++) {
-            if (array[j] < array[min_ind]) {
-                min_ind = j;
-            }
-        }
-        if (min_ind != i) {
-            tmp = array[i];
-            array[i] = array[min_ind];
-            array[min_ind] = tmp;
-            print_array(array,size);
-        }
-    }
+ void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_ind, tmp;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_ind = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_ind])
+				min_ind = j;
+		}
+		if (min_ind != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_ind];
+			array[min_ind] = tmp;
+			print_array(array,size);
+		}
+	}
 }
